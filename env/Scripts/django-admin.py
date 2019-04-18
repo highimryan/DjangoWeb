81592bbcc536aa90fb61dd2df2e54c157c99aa2b
Ryan White <WHITER23@my.erau.edu>
2019-04-18 09:48:40 -0400

    Test
    Test

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\white\OneDrive - Embry-Riddle Aeronautical University\Projects\DjangoWeb\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
